8205837937b8f3fee9199a60a6f553a878d20c0c
amitrsingh1992 <singh.amit.10ee1074@gmail.com>
2016-12-28 19:27:53 +0530

    ipl demo completion

---
+++ script.js
@@ -0,0 +1,49 @@
+$(function () {
+  window.location.href = '/#';
+
+// An event handler with calls the render function on every hashchange.
+	// The render function will show the appropriate content of out page.
+	$(window).on('hashchange', function(){
+		render(decodeURI(window.location.hash));
+	});
+	function render(url) {
+console.log("calling");
+		// Get the keyword from the url.
+		var temp = url.split('/')[0];
+console.log(temp);
+
+		var	map = {
+
+			// The "Homepage".
+			'': function() {
+        $(".mycontent").html('');
+        $(".myheader").html('');
+        putFirstHeader();
+        putFirstContent();
+			},
+
+			// Single Products page.
+			'#team': function() {
+        var teamName = url.split('#team/')[1].trim();
+        $(".mycontent").html('');
+        putTeamContent(teamName);
+      // $(".")
+				// Get the index of which product we want to show and call the appropriate function.
+			},
+			"#teamInfo":function(){
+			console.log("teaminfo")
+		}
+
+		};
+
+		// Execute the needed function depending on the url keyword (stored in temp).
+		if(map[temp]){
+			map[temp]();
+		}
+		// If the keyword isn't listed in the above - render the error page.
+		else {
+window.location.href = '/#';
+		}
+
+	}
+});
---
+++ single.js
@@ -0,0 +1,71 @@
+function putFirstHeader(){
+var dom;
+$.get("template/header.html",function(data){
+ dom = $(data);
+});
+
+var ref =firebase.database().ref();
+
+ref.once("value").then(function(data){
+  var carDOM = "";
+  var imgDOM="";
+$.each(data.val().team_info,function(key,value){
+if(key===0){
+  carDOM+='<li data-target="#myCarousel" data-slide-to="0" class="active"></li>';
+    imgDOM+='<div class="item active"><img src="ipl images/team_logo/'+value.team_img_url+'" alt="Delhi Daredevils" width="460" height="345" /></div>';
+}else{
+  carDOM+='<li data-target="#myCarousel" data-slide-to="'+key+'"></li>';
+    imgDOM+='<div class="item"><img src="ipl images/team_logo/'+value.team_img_url+'" alt="'+value.team_name+'" width="460" height="345" /></div>';
+}
+});
+dom.find(".carousel-indicators").append(carDOM);
+dom.find(".carousel-inner").append(imgDOM);
+$(".myheader").append(dom);
+});
+};
+// <div class="card">
+//   <img class="card-img-top" src="..." alt="Card image cap">
+//   <div class="card-block">
+//     <h4 class="card-title">Card title</h4>
+//   </div>
+// </div>
+function putFirstContent() {
+
+  var ref =firebase.database().ref();
+
+  ref.once("value").then(function(data){
+    var carDOM = "";
+  $.each(data.val().team_info,function(key,value){
+carDOM+=' <div class="card col-lg-3">\
+  <img class="card-img-top" src="ipl images/team_logo/'+value.team_img_url+'" alt="Card image cap">\
+  <div class="card-block">\
+    <h4 class="card-title">'+value.team_name+'</h4>\
+  </div></div>';
+  });
+  $(".mycontent").append(carDOM);
+  });
+}
+
+function putTeamContent(teamName) {
+
+  var ref =firebase.database().ref(teamName);
+
+  ref.once("value").then(function(data){
+    var carDOM = "";
+  $.each(data.val(),function(key,value){
+carDOM+=' <div class="card col-lg-4">\
+  <img class="card-img-top" src="ipl images/'+value.player_img_url+'" alt="Card image cap">\
+    <div class="card-block">\
+    <h4 class="card-title"><strong>Name:</strong>'+value.player_name+'</h4>\
+    <h4 class="card-title"><strong>Role:</strong>'+value.player_role+'</h4>\
+    <h4 class="card-title"><strong>Batting Style:</strong>'+value.player_batting_style+'</h4>\
+    <h4 class="card-title"><strong>Bowling Style:</strong>'+value.player_bowling_style+'</h4>\
+    <h4 class="card-title"><strong>Nationality:</strong>'+value.player_nationality+'</h4>\
+    <h4 class="card-title"><strong>Date Of Birth:</strong>'+value.player_dob+'</h4>\
+  </div></div>';
+  });
+  console.log(carDOM);
+  $(".mycontent").append(carDOM);
+  });
+
+}
